eb0ed21e126a2ddb593aa00d215a626bff875cbb
tejas-130704 <tejasjadhav130704@gmail.com>
2025-07-18 16:29:42 +0530

    host fe

--- frontend/src/components/JoinRoom.jsx
+++ frontend/src/components/JoinRoom.jsx
@@ -48,7 +48,7 @@ export default function JoinRoom() {
     }, [])
 
     useEffect(() => {
-        socketRef.current = new WebSocket('ws://localhost:3000');
+        socketRef.current = new WebSocket('ws://8f83fd459256.ngrok-free.app');
 
         socketRef.current.addEventListener('open', () => {
 
