bc0695d08f0596a42144824a42696b9f7ade5399
tejas-130704 <tejasjadhav130704@gmail.com>
2025-07-18 17:42:15 +0530

    Chnages done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/PaperCheatIO/', 
   plugins: [react(),tailwindcss()],
-  base: '/PaperCheatIO/',
+ 
 })
